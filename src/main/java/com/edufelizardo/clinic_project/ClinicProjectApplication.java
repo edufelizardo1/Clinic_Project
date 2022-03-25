807dd0b7acb5fe41b4380f21c825bcc8422843c6
Cândido <edufelizardo1@gamil.com>
2022-03-25 14:01:06 -0300

    Initial commit

---
+++ src/main/java/com/edufelizardo/clinic_project/ClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.clinic_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edufelizardo/clinic_project/ClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.clinic_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
